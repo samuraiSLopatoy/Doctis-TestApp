dbd8ea32a8d873cda66634d08b97e3527eff6cc6
Mikhail Kulagin <mikhail.kulagin@iss.digital>
2023-04-08 10:20:05 +0300

    Initial Commit

---
+++ Doctis-TestApp/Doctis_TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Doctis_TestAppApp.swift
+//  Doctis-TestApp
+//
+//  Created by Михаил Кулагин on 08.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Doctis_TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
